b44f96b9c5bb05cba91c4f4b694c2744eb74ebfc
Armen <82414784+quakerlegend1@users.noreply.github.com>
2023-03-17 20:48:21 +0300

    '

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          qq
         </p>
         <p>Добрый день снова</p>
         <a
