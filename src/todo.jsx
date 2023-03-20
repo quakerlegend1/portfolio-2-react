964e03e05d46ec454e825bd34d93841929bd1130
Armen <82414784+quakerlegend1@users.noreply.github.com>
2023-03-20 20:10:39 +0300

    The simple 2DoList App
    not completed

--- src/todo.jsx
+++ src/todo.jsx
@@ -8,12 +8,16 @@ function Todolist(props) {
     return (
         <>
         <div className="todo-div">
+            <h1 style={{textAlign: "center"}}>2Do or not 2Do?</h1>
             <div className="todo-div-form">
-                <input className="todo-text form-control" value={value} type="text" placeholder="Что бы вы хотели сделать?"/>
-                <div className=".todo-add" onClick={setValue} ><span className="btn btn-primary">Добавить</span></div>
+                <input className="todo-text form-control" value={value} type="text" placeholder="Что бы вы хотели сделать?" onChange={(e)=>{setValue(e.target.value)}}/>
+                <div className="todo-add" onClick={setValue}><span className="btn btn-primary">Добавить</span></div>
             </div>
             <ul className="todo-ul">
-                <li>пойти куда-то</li>
+                <div style={{display:"flex"}}>
+                    <li>пойти куда-то</li>
+                    <button type="button" className="todo-btn btn btn-danger">delete</button>
+                </div>
             </ul>
         </div>
         </>
